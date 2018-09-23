c3e53c7f05e9ffb213c2a6f3efdcb45378ad51b2
Martin Charles <martincharles07@gmail.com>
2018-09-23 16:25:14 -0400

    Completed 4.10

---
+++ src/Chapter4_10/Main.kt
@@ -0,0 +1,38 @@
+package Chapter4_10
+
+fun main(args: Array<String>) {
+    testRootOnly()
+    test()
+}
+
+fun testRootOnly() {
+    val node = BinaryTreeNode(null)
+    require(node == node.getRandom())
+}
+
+fun test() {
+    val root = BinaryTreeNode(null)
+
+    val left = BinaryTreeNode(root)
+    root.addLeftChild(left)
+
+    val right = BinaryTreeNode(root)
+    root.addRightChild(right)
+
+    val rightRight = BinaryTreeNode(right)
+    right.addRightChild(rightRight)
+
+    require(left == root.getRandom { 0 })
+    require(right == root.getRandom { 1 })
+
+    require(root == root.getRandom { 4 })
+    var switch = false
+    require(rightRight == root.getRandom {
+        if (!switch) {
+            switch = true
+            1
+        } else {
+            0
+        }
+    })
+}
