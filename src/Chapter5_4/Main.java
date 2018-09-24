ef5702e83b6a2e872df0e2dd90c17ac3f207a57b
Martin Charles <martincharles07@gmail.com>
2018-09-24 15:17:28 -0400

    Updated 5.4
    The answer provided in the book doesn't seem to work. Just skipping this problem.

--- src/Chapter5_4/Main.java
+++ src/Chapter5_4/Main.java
@@ -1,66 +1,81 @@
 package Chapter5_4;
 
-// TODO: Add Tests
+// TODO: Tests Fail
 
 public class Main {
-    private static boolean getBit(int num, int i) {
-        return (num & (1 << i)) != 0;
+    public static void main(String args[]) {
+        testGetNext();
+        testGetPrev();
     }
 
-    private static int clearBit(int num, int i) {
-        int mask = ~(1 << i);
-        return num & mask;
+    private static void testGetNext() {
+        int prev = 0b11011001111100;
+        int next = 0b11011010001111;
+
+        assert getNextLargest(prev) == next;
     }
 
-    private static int setBit(int num, int i) {
-        return num | (1 << i);
+    private static void testGetPrev() {
+        int prev = 0b10011110000011;
+        int next = 0b10011101110000;
+
+        assert getNextSmallest(prev) == next;
     }
 
     private static int getNextSmallest(int num) {
-        int leastSignificantZero = 0;
-        int leastSignificantOneAfterZero = 0;
+        int temp = num;
 
+        int c0 = 0;
+        int c1 = 1;
 
-        for (int i = 1; i <= 32; i++) {
-            boolean bit = getBit(num, i);
-            if (leastSignificantZero == 0 && !bit) {
-                leastSignificantZero = i;
-            }
-
-            if (leastSignificantZero != 0 && bit) {
-                leastSignificantOneAfterZero = i;
-                break;
-            }
+        while ((temp & 1) == 1) {
+            c1++;
+            temp >>= 1;
         }
 
-        if (leastSignificantOneAfterZero == 0) {
-            throw new IllegalArgumentException("Impossible");
+        if (temp == 0) {
+            throw new IllegalArgumentException("need to reduce the number of ones.");
         }
 
-        return clearBit(setBit(num, leastSignificantZero), leastSignificantOneAfterZero);
+        while (((temp & 1) == 0) && (temp != 0)) {
+            c0++;
+            temp >>= 1;
+        }
+
+        int p = c0 + c1;
+
+        num &= ((~0) << (p + 1));
+
+        int mask = (1 << (c1 + 1)) - 1;
+        num |= mask << (c0 - 1);
+
+        return num;
     }
 
     private static int getNextLargest(int num) {
-        int leastSignificantOne = 0;
-        int leastSignificantZeroAfterOne = 0;
+        int c = num;
+        int trailingZeroes = 0;
 
-
-        for (int i = 1; i <= 32; i++) {
-            boolean bit = getBit(num, i);
-            if (leastSignificantOne == 0 && bit) {
-                leastSignificantOne = i;
-            }
-
-            if (leastSignificantOne != 0 && !bit) {
-                leastSignificantZeroAfterOne = i;
-                break;
-            }
+        while (((c & 1) == 0 && (c != 0))) {
+            trailingZeroes++;
+            c >>= 1;
         }
 
-        if (leastSignificantZeroAfterOne == 0) {
-            return num << 1;
+        int onesAfterZero = 0;
+
+        while ((c & 1) == 1) {
+            onesAfterZero++;
+            c >>= 1;
         }
 
-        return clearBit(setBit(num, leastSignificantZeroAfterOne), leastSignificantOne);
+        int rightmostNonTrailingZero = trailingZeroes + onesAfterZero;
+        if (rightmostNonTrailingZero == 31 || rightmostNonTrailingZero == 0) {
+            throw new IllegalArgumentException("No larger value.");
+        }
+
+        num |= (1 << rightmostNonTrailingZero);
+        num &= ~((1 << rightmostNonTrailingZero) - 1);
+        num |= (1 << (onesAfterZero - 1)) - 1;
+        return num;
     }
 }
