f23f4046e6c911304675cf31e03a23286e14f49a
Martin Charles <martincharles07@gmail.com>
2018-09-24 00:20:11 -0400

    Completed 5.6

---
+++ src/Chapter5_6/Main.java
@@ -0,0 +1,26 @@
+package Chapter5_6;
+
+public class Main {
+    public static void main(String []args) {
+        assert flipDistance(29, 15) == 2;
+    }
+
+    private static boolean getBit(int num, int i) {
+        return (num & (1 << i)) != 0;
+    }
+
+    public static int flipDistance(int a, int b) {
+        int distance = 0;
+
+        for (int i = 1; i <= 32; i++) {
+            boolean aBit = getBit(a, i);
+            boolean bBit = getBit(b, i);
+
+            if (aBit != bBit) {
+                distance++;
+            }
+        }
+
+        return distance;
+    }
+}
