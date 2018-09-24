06880208dced395fa450676e68c8d824280f656f
Martin Charles <martincharles07@gmail.com>
2018-09-24 00:32:17 -0400

    Completed 5.7
    Should probably generate the 32 bit strings.

---
+++ src/Chapter5_7/Main.java
@@ -0,0 +1,15 @@
+package Chapter5_7;
+
+public class Main {
+    public static void main(String[] args) {
+        assert pairwiseSwap(0b101010) == 0b010101;
+        assert pairwiseSwap(0b111110) == 0b111101;
+    }
+
+    private static int pairwiseSwap(int num) {
+        int evenMask = 0b10101010101010101010101010101010;
+        int oddMask =  0b01010101010101010101010101010101;
+
+        return (num & evenMask) >> 1 | (num & oddMask) << 1;
+    }
+}
